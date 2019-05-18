86cd83a9533c1c953c004d67556f8541a18457d9
lovdeepm <lovdeep@gmail.com>
2019-05-18 14:19:20 -0700

    added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css'
 
 import Navbar from './components/Navbar/Navbar'
 import Login from './components/Login/Login'
