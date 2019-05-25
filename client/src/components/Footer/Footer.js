58272ac1d0e62c95611b09d2eb59ce7eb83452f5
lovdeepm <lovdeep@gmail.com>
2019-05-25 12:38:48 -0700

    changes to frontend pages

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Footer.css';
 
 const Footer = (props) => {
         return (
             <footer className="footer mt-auto py-3">
                 <div className="container">
-                    <span className="footerText">BetMo! &copyright; All rights reserved 2019</span>
+                    <span className="footerText">BetMo! <i class="far fa-copyright"></i> All rights reserved 2019</span>
                 </div>
             </footer>
         );
---
+++ routes/api-routes/utility.js
@@ -0,0 +1,19 @@
+
+const router = require("express").Router();
+const gamesController = require("../../controllers/gamesController");
+
+// Matches with "/api/games"
+router.route("/")
+  .get(gamesController.findAll)
+  .post(gamesController.create);
+
+// Matches with "/api/games/:id"
+router
+  .route("/:id")
+  .get(gamesController.findById)
+  .put(gamesController.update)
+  .delete(gamesController.remove);
+
+
+
+module.exports = router;
