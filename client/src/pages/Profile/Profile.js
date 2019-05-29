e6ffe4623ded92d6703730f1a26b6c523a87582d
Matt Sainson <mattsainson@gmail.com>
2019-05-28 19:13:34 -0700

    Wrapped App render in <>; changed dashboard to Dashboard

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import jwt_decode from 'jwt-decode'
+import Transactions from '../../components/Transactions'
 
 class Profile extends Component {
     constructor() {
@@ -23,6 +24,7 @@ class Profile extends Component {
 
     render () {
         return (
+            <>
             <div className="container">
                 <div className="jumbotron mt-5">
                     <div className="col-sm-8 mx-auto">
@@ -47,6 +49,7 @@ class Profile extends Component {
                 </div>
             </div>
             <Transactions />
+            </>
         )
     }
 }
