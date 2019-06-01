b42182a027f7e35ab1e49d1507e23ba0e7c6f97c
lovdeepm <lovdeep@gmail.com>
2019-06-01 14:25:39 -0700

    changes to login

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -31,7 +31,7 @@ class Login extends Component {
         }
 
         login(user).then(res => {
-            if (!res.error)  {
+            if (!res)  {
                 this.props.history.push(`/dashboard`)
             } else {
                 this.setState({error: res.error})
